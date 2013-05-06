b1e721dbea932776bee36af13d40b4b1066e0c3d
Timm Simpkins <poduck@gmail.com>
2013-05-06 00:46:49 -0600

    Rewrite of entire code.  Works pretty well, but not propagating properly.

---
+++ VaccDemo/GraphInfect.py
@@ -0,0 +1,125 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+
+import Logic
+import newvacc
+
+class GraphInfect(QDialog, newvacc.Ui_Dialog, Logic.Matrix):
+    
+    def __init__(self, parent=None):
+        super(GraphInfect, self).__init__(parent)
+        self.setupUi(self)
+
+        self.primeButton.clicked.connect(self.primePressed)
+        self.infectButton.clicked.connect(self.infectPressed)
+        self.matrixWidthBox.valueChanged.connect(self.matrixWidthSpinnerChanged)
+        self.numInfectedBox.valueChanged.connect(self.infectedSpinnerChanged)
+        self.numVaccinatedBox.valueChanged.connect(self.vaccinatedSpinnerChanged)
+        self.chanceUnvacInfectedBox.valueChanged.connect(self.updateValues)
+        self.chanceVacInfectedBox.valueChanged.connect(self.updateValues)
+
+        self.colors = { "vaccinated":Qt.green, "unvaccinated":Qt.gray, "infected":Qt.red, "vacImmune":Qt.darkGreen, "unvacImmune":Qt.darkGray, "vacInfected":Qt.darkMagenta, "unvacInfected":Qt.magenta, "vacLucky":Qt.darkGreen, "unvacLucky":Qt.darkGray }
+        self.lastPressed = None
+        self.matrixWidthSpinnerChanged()
+
+    def primePressed(self):
+        self.lastPressed = "prime"
+        self.prime()
+        self.update()
+
+    def infectPressed(self):
+        if not self.lastPressed in ["infect","prime"]:
+            QMessageBox.information(self, "Error", "You must prime the matrix first!")
+        else:
+            self.propagate()
+            self.lastPressed = "infect"
+            self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+
+        qp.setBrush(Qt.white)
+        qp.setPen(Qt.transparent)
+        if self.lastPressed == "prime":
+            self.drawPrimed(qp)
+        elif self.lastPressed == "infect":
+            self.infect(qp)
+        qp.end()
+
+    def prime(self):
+        Logic.Matrix.prime(self, self.matrixWidthBox.value()**2, self.numVaccinatedBox.value(), self.numInfectedBox.value(), self.chanceVacInfectedBox.value(), self.chanceUnvacInfectedBox.value())
+
+        self.areaWidth = 435 / self.cardinalWidth
+        self.plotStartX = 10
+        self.plotStartY = 220
+        self.widthX = 561
+        self.heightY = 441 
+        
+        self.startX = self.plotStartX + (self.widthX - (self.areaWidth * self.cardinalWidth))/2
+        self.startY = self.plotStartY + (self.heightY - (self.areaWidth * self.cardinalWidth)) / 2
+        
+        self.update()
+
+    def infect(self, qp):
+        self.drawInfected(qp)
+        
+    def drawPrimed(self, qp):
+        qp.setBrush(Qt.white)
+        qp.drawRect(self.plotStartX, self.plotStartY, self.widthX, self.heightY)
+        for x in range(self.cardinalWidth):
+            for y in range(self.cardinalWidth):
+                self.plot([x,y], self.colors[self._matrix[x][y]], qp)
+    
+    def drawInfected(self, qp):
+        qp.setBrush(Qt.white)
+        qp.drawRect(self.plotStartX, self.plotStartY, self.widthX, self.heightY)
+        for x in range(self.cardinalWidth):
+            for y in range(self.cardinalWidth):
+                self.plot([x,y], self.colors[self.getFinal([x,y])], qp)
+
+    def getPoint(self, position):
+        (x, y) = position
+        pointX = x * self.areaWidth + self.startX
+        pointY = y * self.areaWidth + self.startY
+        return [pointX, pointY]
+
+    def plot(self, position, color, qp):
+        qp.setBrush(color)
+        (x,y) = self.getPoint(position)
+        qp.drawRect(x, y, self.areaWidth - 1, self.areaWidth - 1)
+    
+    def infectedSpinnerChanged(self):
+        if self.numInfectedBox.value() + self.numVaccinatedBox.value() > self._numPeople:
+            self.numVaccinatedBox.setValue(self._numPeople - self.numInfectedBox.value())
+        self.updateValues()
+    
+    def vaccinatedSpinnerChanged(self):
+        if self.numInfectedBox.value() + self.numVaccinatedBox.value() > self._numPeople:
+            self.numInfectedBox.setValue(self._numPeople - self.numVaccinatedBox.value())
+        self.updateValues()
+
+    def matrixWidthSpinnerChanged(self):
+        width = self.matrixWidthBox.value()**2 - 1
+        self.numInfectedBox.setMaximum(width)
+        self.numVaccinatedBox.setMaximum(width)
+        self.updateValues()
+
+    def updateValues(self):
+        self._numPeople = self.matrixWidthBox.value() ** 2
+        self._numVaccinated = self.numVaccinatedBox.value()
+        self._numInfected = self.numInfectedBox.value()
+        self._chanceVacInfected = self.chanceVacInfectedBox.value()
+        self._chanceUnvacInfected = self.chanceUnvacInfectedBox.value()
+        self.showVacNum.setText(str(self._numVaccinated))
+        self.showUnvacNum.setText(str(self._numPeople - self._numVaccinated - self._numInfected))
+        self.showVacVuln.setText(str(int(round(self._chanceVacInfected / 100 * self._numVaccinated))))
+        self.showUnvacVuln.setText(str(int(round(self._chanceUnvacInfected/100 * (self._numPeople - self._numVaccinated - self._numInfected)))))
+
+
+
+app = QApplication(sys.argv)
+form = GraphInfect()
+form.show()
+app.exec_()
---
+++ VaccDemo/Logic.py
@@ -0,0 +1,141 @@
+'''
+Created on May 5, 2013
+
+@author: poduck
+'''
+from random import randrange
+from math import sqrt
+
+class Matrix:
+    def __init__(self):
+        pass
+
+    def prime(self, numPeople, numVaccinated, numInfected, chanceVacInfected, chanceUnvacInfected):
+        """
+        Initialize all matrices.
+        Determines position of initially infected, unvaccinated, and
+        healthy people
+        """
+        self._directions = [[1,1],[0,1],[2,1],[1,0],[2,0],[1,2],[0,2],[2,2]]
+        # Number of people in matrix.  Immutable outside prime method.
+        self._numPeople = numPeople
+        # Number of infected people.  Immutable outside prime method.
+        self._numInfected = numInfected
+        # Number of vaccinated people.  Immutable outside of prime method.
+        self._numVaccinated = numVaccinated
+        # Chance a vaccinated person will become infected.
+        self._chanceVacInfected = chanceVacInfected
+        # Chance an unvaccinated person will become infected.
+        self._chanceUnvacInfected = chanceUnvacInfected
+        # width of the matrix in number of people.  (All matrices are square.)
+        self.cardinalWidth = int(sqrt(numPeople))
+        # non-cardinal width.  Starts with 0. 
+        self.width = self.cardinalWidth - 1
+        
+        # fill _matrix with unvaccinated people
+        self._matrix = [["unvaccinated" for x in range(self.cardinalWidth)] for y in range(self.cardinalWidth)]
+
+        # set infected people
+        count = 0
+        while count < self._numInfected:
+            x = randrange(0, self.cardinalWidth)
+            y = randrange(0, self.cardinalWidth)
+            if self._matrix[x][y] == "unvaccinated":
+                self._matrix[x][y] = "infected"
+                count += 1
+        # set vaccinated people
+        while count < self._numVaccinated:
+            x = randrange(0, self.cardinalWidth)
+            y = randrange(0, self.cardinalWidth)
+            if self._matrix[x][y] == "unvaccinated":
+                self._matrix[x][y] = "vaccinated"
+                count += 1
+
+    def checkOverlap(self, direction, position):
+        """
+        Check if an edge is overlapped.
+        if less than zero, the position goes to maximum.
+        if greater than maximum, position set to zero.
+        returns the new position.
+        """
+        if direction == 1:
+            if position == 0:
+                return self.width
+            else:
+                return (position - 1)
+        if direction == 2:
+            if position == self.width:
+                return 0
+            else:
+                return (position + 1)
+        return position
+    
+    def changePosition(self, startingPoint, direction):
+        """
+        Takes in a starting point [x,y] and a direction [x,y] where
+        1 = <
+        2 = >
+        0 = unchanged
+        returns the new matrix position.
+        """
+        (x,y) = startingPoint
+        x = self.checkOverlap(direction[0], x)
+        y = self.checkOverlap(direction[1], y)
+        return [x,y]
+    
+    def getsInfected(self, status):
+        if status == "infected":
+            return None
+        if status == "vaccinated":
+            chance = int(round(self._chanceVacInfected * 10))
+        else:
+            chance = int(round(self._chanceUnvacInfected * 10))
+        if chance > randrange(1, 1001):
+            return True
+        return False
+    
+    def spread(self, position):
+        for direction in self._directions:
+            (x, y) = self.changePosition(position, direction)
+            if self.canInfectMatrix[x][y] and not self.contactMatrix[x][y]:
+                self.canInfectMatrix[x][y] = False
+                self.contactMatrix[x][y] = True
+                self.spread([x,y])
+    
+    def propagate(self):
+        # initialize canInfectMatrix and contactMatrix
+        self.contactMatrix = [[False for row in range(self.cardinalWidth)] for col in range(self.cardinalWidth)]
+        self.canInfectMatrix = [[self.getsInfected(self._matrix[col][row]) for row in range(self.cardinalWidth)] for col in range(self.cardinalWidth)]
+        print self.canInfectMatrix
+        # Spread infection.
+        for x in range(self.cardinalWidth):
+            for y in range(self.cardinalWidth):
+                if self._matrix[x][y] == "infected":
+                    self.spread([x,y])
+    
+    def getFinal(self, position):
+        """
+        infected = infected
+        Vaccinated, immune, contacted = UnvacImmune
+        Unvaccinated, immune, contacted = VacImmune
+        Vaccinated, not immune, contacted = VacInfected
+        Unvaccinated, not immune, contacted = UnvacInfected
+        Vaccinated, not immune, not contacted = VacLucky
+        unvaccinated, not immune, not contacted = UnvacLucky
+        """
+        (x, y) = position
+        if self._matrix[x][y] == "infected":
+            return "infected"
+        if self.contactMatrix[x][y]:
+            if self.canInfectMatrix[x][y]:
+                if self._matrix[x][y] == "vaccinated":
+                    return "vacInfected"
+                return "unvacInfected"
+            if self._matrix[x][y] == "vaccinated":
+                return "vacImmune"
+            return "unvacImmune"
+        if self._matrix[x][y] == "vaccinated":
+            return "vacLucky"
+        return "unvacLucky"
+                    
+                    
